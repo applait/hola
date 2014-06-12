ddd9b63e8a9bda195a87aad74a92cbadadd7e78a
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-06-12 21:00:52 +0530

    Fix fading of navbar in mobile screens and improve responsiveness on page load

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+// Prepare the collapse nav function
+var collapsenav = function() {
     if ($(".navbar").offset().top > $('.intro').outerHeight() - 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+};
+
+//Collapse navigation on page load depending on position of page
+$(window).load(collapsenav);
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(collapsenav);
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
